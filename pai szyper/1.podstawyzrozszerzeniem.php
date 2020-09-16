08f7b0d996f7ad67efc91ed2450c976ba630d971
MarcinCzaplicki <65462131+MarcinCzaplicki@users.noreply.github.com>
2020-09-16 10:28:38 +0200

    Update 1.podstawyzrozszerzeniem.php

--- pai szyper/1.podstawyzrozszerzeniem.php
+++ pai szyper/1.podstawyzrozszerzeniem.php
@@ -5,4 +5,53 @@
   echo 'Cześć $name'.'<br>';
   echo "Cześć $name";
   //echo 'test';
+  //typy danych
+//boolean
+$prawda=true;
+$falsz=false;
+echo $prawda;
+echo $falsz;
+
+//integer
+$bin=0b010101;
+echo "</br>$bin";
+$oct=012;
+echo "</br>$oct";
+$dec=12;
+echo "</br>$dec";
+$hex=0xff;
+echo "</br>$hex</br></br>";
+
+$name='Anna';
+
+$text=<<<LABEL
+<hr>
+Heredoc 1
+<br>
+Imię: $name
+<hr>
+LABEL;
+
+echo $text;
+
+echo <<<L
+    Heredoc 2<br>
+    Imię: $name
+    <br>
+    Nazwisko: $surname
+    <hr>
+L;
+
+//skladnia nowdoc
+echo <<<'L'
+    Nowdoc<br>
+    Imię: $name
+    <br>
+    Nazwisko: $surname
+    <hr>
+L;
+
+$city='Poznań';
+echo "Nazwa miasta: \$city wartość city $city";
+
 ?>
